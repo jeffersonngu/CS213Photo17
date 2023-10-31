800f5a053fb1fbabf395d23903d61641996cc30c
Jefferson Nguyen <98124001+Jefferson-Nguyen@users.noreply.github.com>
2023-10-31 13:07:18 -0400

    Initial Commit

---
+++ src/photos/Photos.java
@@ -0,0 +1,16 @@
+package photos;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Photos extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
