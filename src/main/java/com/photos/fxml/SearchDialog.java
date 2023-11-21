7e0e4f90de7a23a2a62954bcb4ced69ec75c82ef
Jefferson Nguyen <98124001+Jefferson-Nguyen@users.noreply.github.com>
2023-11-20 21:54:19 -0500

    SearchDialog combinations disappearing every 2 characters fix

--- src/main/java/com/photos/fxml/SearchDialog.java
+++ src/main/java/com/photos/fxml/SearchDialog.java
@@ -107,17 +107,21 @@ public class SearchDialog extends Dialog<List<Photo>> {
         boolean tag2Active = searchDialogController.tag2_1.getValue() != null && !searchDialogController.tag2_2.getText().isBlank();
         boolean dateActive = searchDialogController.date1.getValue() != null && searchDialogController.date2.getValue() != null;
 
-        if (tag1Active && tag2Active && !searchDialogController.combination1.isVisible()) {
-            searchDialogController.combination1.setVisible(true);
-            searchDialogController.combination1.setValue("AND");
+        if (tag1Active && tag2Active) {
+            if (!searchDialogController.combination1.isVisible()) {
+                searchDialogController.combination1.setVisible(true);
+                searchDialogController.combination1.setValue("AND");
+            }
         } else {
             searchDialogController.combination1.setVisible(false);
             searchDialogController.combination1.setValue("AND");
         }
 
-        if ((tag1Active || tag2Active) && dateActive && !searchDialogController.combination2.isVisible()) {
-            searchDialogController.combination2.setVisible(true);
-            searchDialogController.combination2.setValue("AND");
+        if ((tag1Active || tag2Active) && dateActive) {
+            if (!searchDialogController.combination2.isVisible()) {
+                searchDialogController.combination2.setVisible(true);
+                searchDialogController.combination2.setValue("AND");
+            }
         } else {
             searchDialogController.combination2.setVisible(false);
             searchDialogController.combination2.setValue("AND");
